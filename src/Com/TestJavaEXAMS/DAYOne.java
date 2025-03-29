2dd56c21c3e229bcae8a69c6b3c35907e86d46ad
PrakashRaj1703 <prakashpaiyya@gmail.com>
2025-03-29 11:58:12 +0530

    New Code

---
+++ src/Com/TestJavaEXAMS/DAYOne.java
@@ -0,0 +1,35 @@
+package Com.TestJavaEXAMS;
+
+public class DAYOne {
+	private void tc1() {
+		String s = "AGNI";
+		String temp = "";
+		int length = s.length();
+		for (int i = length - 1; i >= 0; i--) {
+			char t = s.charAt(i);
+			temp = temp + t;
+
+		}
+		System.out.println(temp);
+	}
+
+	private void tc2() {
+
+		String s = "StringBuffer";
+		StringBuilder s1 = new StringBuilder();
+		StringBuilder append = s1.append(s);
+		StringBuilder reverse = append.reverse();
+
+		System.out.println(append);
+	}
+
+	public static void main(String[] args) {
+		String s = "StringBuffer";
+		StringBuilder s1 = new StringBuilder();
+		StringBuilder append = s1.append(s);
+		StringBuilder reverse = append.reverse();
+
+		System.out.println(reverse);
+
+	}
+}

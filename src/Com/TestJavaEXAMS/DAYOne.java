3ce45ffdf60c7e4062e579e2a7571e012b5a1b0f
PrakashRaj1703 <prakashpaiyya@gmail.com>
2025-03-29 20:43:28 +0530

    Updated String codes

--- src/Com/TestJavaEXAMS/DAYOne.java
+++ src/Com/TestJavaEXAMS/DAYOne.java
@@ -23,7 +23,7 @@ public class DAYOne {
 		System.out.println(append);
 	}
 
-	public static void main(String[] args) {
+	private void tc3() {
 		String s = "StringBuffer";
 		StringBuilder s1 = new StringBuilder();
 		StringBuilder append = s1.append(s);
@@ -31,5 +31,53 @@ public class DAYOne {
 
 		System.out.println(reverse);
 
+	}
+
+	private void tc4() {
+		String s = "racecar123@#$%^gfdggfgfg";
+		String temp = "";
+		// String o = s;
+		int l = s.length();
+		for (int i = l - 1; i >= 0; i--) {
+			char t = s.charAt(i);
+
+			temp += t;
+
+		}
+		System.out.println(temp);
+		if (s.equals(temp)) {
+			System.out.println("Palindrom");
+		} else {
+			System.out.println("Not Palindrom");
+		}
+	}
+
+	private void tc5() {
+		String s = "racecar123@#$%^gfdggfgfg";
+		StringBuilder s1 = new StringBuilder();
+		StringBuilder s2 = new StringBuilder();
+		StringBuilder s3 = new StringBuilder();
+
+		for (char ch : s.toCharArray()) {
+			if (Character.isLetter(ch)) {
+
+				s1.append(ch);
+
+			} else if (Character.isDigit(ch)) {
+				s2.append(ch);
+
+			} else {
+
+				s3.append(ch);
+			}
+
+		}
+		System.out.println("Charcter is :" + "\n" + s1);
+		System.out.println("Number is " + "\n" + s2);
+		System.out.println("Special Char is :" + "\n" + s3);
+	}
+
+	public static void main(String[] args) {
+		
 	}
 }
